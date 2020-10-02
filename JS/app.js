a4bf28c3a899c0ab9e7b966415b21d333180b7de
Kundena Rahul <55629530+rahulkundena@users.noreply.github.com>
2020-10-02 20:03:40 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    // card1
+    var card = document.getElementById('dark-mode1');
+    card.classList.toggle("dark");
+    // card2
+    var card = document.getElementById('dark-mode2');
+    card.classList.toggle("dark");
+    // card3
+    var card = document.getElementById('dark-mode3');
+    card.classList.toggle("dark");
+    // card4
+    var card = document.getElementById('dark-mode4');
+    card.classList.toggle("dark");
+    // card5
+    var card = document.getElementById('dark-mode5');
+    card.classList.toggle("dark");
+    // card6
+    var card = document.getElementById('dark-mode6');
+    card.classList.toggle("dark");
+    // card7
+    var card = document.getElementById('dark-mode7');
+    card.classList.toggle("dark");
+    // card8
+    var card = document.getElementById('dark-mode8');
+    card.classList.toggle("dark");
+    // card9
+    var card = document.getElementById('dark-mode9');
+    card.classList.toggle("dark");
+    // card10
+    var card = document.getElementById('dark-mode10');
+    card.classList.toggle("dark");
+    // card 11
+    var card = document.getElementById('dark-mode11');
+    card.classList.toggle("dark");
+    // card12
+    var card = document.getElementById('dark-mode12');
+    card.classList.toggle("dark");
+    // text
+    var text = document.getElementsByClassName('dark-mode');
+    text.classList.toggle("mode");
+
+ }
+ 
